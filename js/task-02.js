696dec433040666b8aa21222d12e0044ba944173
AlexandraKrosnik <alexandra.krosnik@gmail.com>
2022-01-24 14:03:42 +0200

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+let ingredientsEl = document.querySelector("#ingredients");
+let itemArr = ingredients.map(ingredient => createItem(ingredient));
+
+ingredientsEl.append(...itemArr);
+
+function createItem(ingredient) {
+  let item = document.createElement("li");
+  item.classList.add("item");
+  item.textContent = ingredient;  
+  return item;
+}
+
