91464dfd6dd49768ae6cd8dcb284b4c7e5e9f4a4
AlexandraKrosnik <alexandra.krosnik@gmail.com>
2022-01-25 11:09:29 +0200

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const ref = {
+  nameColorSpan: document.querySelector(".color"),
+  buttonChangeColor: document.querySelector(".change-color"),
+  body: document.body,
+}
+
+const onColorBackgroundChange = () => {
+  let color = getRandomHexColor();
+  ref.body.style.backgroundColor = color;
+  ref.nameColorSpan.textContent = color;
+}
+
+ref.buttonChangeColor.addEventListener("click", onColorBackgroundChange);
+
+
+
