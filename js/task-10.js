affa22c1e06aa90de6e20fe9bb11378cfe7e31b9
AlexandraKrosnik <alexandra.krosnik@gmail.com>
2022-01-25 11:56:26 +0200

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,56 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const ref = {
+  inputNumber: document.querySelector('[type="number"]'),
+  buttonCreate: document.querySelector("[data-create]"),
+  buttonDestroy: document.querySelector("[data-destroy]"),
+  boxes: document.getElementById("boxes"),
+}
+let amountDiv;
+let arrDiv;
+let widthHeigthDiv;
+
+ref.buttonCreate.addEventListener("click", onMakeCollection);
+ref.buttonDestroy.addEventListener("click", destroyBoxes);
+
+const valueFromInput = () => ref.inputNumber.value === "" ? alert("Введіть число!") : amountDiv = Number(ref.inputNumber.value);
+
+const defaultValue = () => {
+  amountDiv = 0;
+  arrDiv = [];
+  widthHeigthDiv = 30;
+}
+
+function createBoxes(amount) {
+  if (amount != 0)
+  {
+    for (let i = 0; i < amount; i += 1)
+    {
+      let div = document.createElement("div");
+      div.style.width = `${widthHeigthDiv}px`;
+      div.style.height = `${widthHeigthDiv}px`;
+      div.style.backgroundColor = getRandomHexColor();
+
+      arrDiv.push(div);
+      
+      widthHeigthDiv += 10;
+    }
+  }
+}
+
+function onMakeCollection() {
+
+  defaultValue();
+
+  valueFromInput();
+  createBoxes(amountDiv); 
+
+  ref.inputNumber.value = "";
+  
+  ref.boxes.append(...arrDiv);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
