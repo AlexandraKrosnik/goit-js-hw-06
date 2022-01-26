1289ab849fb2bdb5a118230a3e011ff5b730f733
AlexandraKrosnik <alexandra.krosnik@gmail.com>
2022-01-26 10:37:08 +0200

    fixed-6-7

--- js/task-06.js
+++ js/task-06.js
@@ -5,10 +5,15 @@ const INPUT_LENGTH = inputEl.getAttribute('data-length');
 
 const inputClassList = inputEl.classList;
 
-const correctInputValueLength = () => inputClassList.contains("invalid") ? inputClassList.replace("invalid", "valid") : inputClassList.add("valid");
-const incorrectInputValueLength = () => inputClassList.contains("valid") ? inputClassList.replace("valid", "invalid") : inputClassList.add("invalid");
+const correctInputValueLength = () => inputClassList.contains("invalid") ?
+    inputClassList.replace("invalid", "valid") :
+    inputClassList.add("valid");
+const incorrectInputValueLength = () => inputClassList.contains("valid") ?
+    inputClassList.replace("valid", "invalid") :
+    inputClassList.add("invalid");
 
-const onCheckInputLength = () => inputEl.value.length <= INPUT_LENGTH ? correctInputValueLength() : incorrectInputValueLength();
+const onCheckInputLength = () => inputEl.value.length == INPUT_LENGTH ?
+    correctInputValueLength() :
+    incorrectInputValueLength();
 
 inputEl.addEventListener("blur", onCheckInputLength);
-console.log(inputEl.value.length === INPUT_LENGTH )
--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const inputRangeEl = document.querySelector("#font-size-control");
 const textEl = document.querySelector("#text");
-
+textEl.style.fontSize = `${inputRangeEl.value}px`;
 const onFontSizeChenge = (event) => textEl.style.fontSize = `${event.currentTarget.value}px`;
 
 inputRangeEl.addEventListener("input", onFontSizeChenge);
